827de4c57b62a8a6b65796da791c7c58de50bfa4
mxdavis <md01@me.com>
2017-08-15 13:59:57 +0300

    mix reverse

---
+++ lib/reverse.ex
@@ -0,0 +1,18 @@
+defmodule Reverse do
+  @moduledoc """
+  Documentation for Reverse.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Reverse.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/reverse_test.exs
@@ -0,0 +1,8 @@
+defmodule ReverseTest do
+  use ExUnit.Case
+  doctest Reverse
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
